4809a3acbaeade95a82889a38da5a91c00b8041f
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-16 23:42:01 -0400

    create file which will contain various API routes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const PORT = 3000;
-
+const api = require('./routes/api');
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,6 +10,8 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send('Hello from server!')
 })
+
+app.use('/api', api);
 
 app.listen(PORT, () => {
     console.log('Server running on localhost:' + PORT);
