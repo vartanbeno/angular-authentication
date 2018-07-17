242bf615cacd5aa0b0f7eb40f5df80c4bd9a4d70
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-17 00:36:43 -0400

    api call for user login

--- server/routes/api.js
+++ server/routes/api.js
@@ -18,9 +18,27 @@ router.get('/', (req, res) => {
 router.post('/register', (req, res) => {
     let userData = req.body;
     let user = new User(userData);
-    user.save((error, registeredUser) => {
-        if (error) throw err;
+    user.save((err, registeredUser) => {
+        if (err) throw err;
         return res.status(200).send(registeredUser);
+    })
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body;
+    User.findOne({ email: userData.email }, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            res.status(401).send('Invalid email.');
+        }
+        else {
+            if (user.password !== userData.password) {
+                res.status(401).send('Invalid password.');
+            }
+            else {
+                res.status(200).send(user);
+            }
+        }
     })
 })
 
