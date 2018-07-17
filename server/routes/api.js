9ef1c9cb2f1a71578316ff7ea62174074014507d
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-17 00:20:45 -0400

    connect to db and create api call to register user

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const User = require('../models/users');
+
+const url = 'mongodb://test:test123@ds139921.mlab.com:39921/angular-auth-events';
+
+mongoose.connect(url, err => {
+    if (err) throw err;
+    console.log('Connected to mongoDB');
+})
+
 router.get('/', (req, res) => {
     res.send('Hello from API route');
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registeredUser) => {
+        if (error) throw err;
+        return res.status(200).send(registeredUser);
+    })
 })
 
 module.exports = router;
