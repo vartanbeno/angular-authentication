adf907846ac04dfb2a8466c4e237b975b64efa64
vartanbeno <vartanbeno@gmail.com>
2018-07-17 15:15:15 -0400

    store token in browser's local storage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,18 @@ export class LoginComponent implements OnInit {
 
   loginUserData: Object = {};
 
-  constructor(private _auth: AuthService) { }
+  constructor(private _auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   loginUser() {
     this._auth.loginUser(this.loginUserData).subscribe(
-      res => console.log(res),
+      res => {
+        console.log(res);
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['special']);
+      },
       err => console.log(err)
     )
   }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +11,18 @@ export class RegisterComponent implements OnInit {
 
   registerUserData: Object = {};
 
-  constructor(private _auth: AuthService) { }
+  constructor(private _auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   registerUser() {
     this._auth.registerUser(this.registerUserData).subscribe(
-      res => console.log(res),
+      res => {
+        console.log(res);
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['special']);
+      },
       err => console.log(err)
     )
   }
